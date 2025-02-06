f8bad45e6fea80ebf2d1e6fb5fbd0b2e71274f70
Yash Personal <thisisyashs@gmail.com>
2025-02-06 18:36:55 +0530

    Learnt Generics

--- typescriptSetup/src/index.ts
+++ typescriptSetup/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello world");
+interface lengthWise {
+  length: number;
+}
+
+function logging<T extends Record<any, any>>(arg: T): T {
+  console.log(arg.length);
+  return arg;
+}
+
+// logging("adasd")
